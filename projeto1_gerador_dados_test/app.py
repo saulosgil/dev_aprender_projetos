b19c6b9ad5cc0c0dadcf8b9629a632f9e425d623
saulosgil <saulosgil@hotmail.com>
2024-05-28 18:27:12 -0300

    update: app.py and insert projeto_orientacoes

---
+++ projeto1_gerador_dados_test/app.py
@@ -0,0 +1,43 @@
+## Bibliotecas
+import random
+import os
+
+## Listas 
+# Nomes
+names = ["John",
+         "Emily", 
+         "Michael",
+         "Sophia",
+         "David"
+         ]
+
+# E-mails
+emails = ["John@email.com",
+         "Emily@email.com", 
+         "Michael@email.com",
+         "Sophia@email.com",
+         "David@email.com"
+         ]
+
+# Telefones
+phone_numbers = ["+1 555-123-4567", 
+                 "+1 555-987-6543",
+                 "+1 555-456-7890",
+                 "+1 555-234-5678",
+                 "+1 555-876-5432"
+                 ]
+
+# Cidades
+cities = ["São Bernanrdo do Campo", "Rio Preto", "Salvador", "Brasília", "Fortaleza"]
+
+# Estados
+states = ["São Paulo", "Rio de Janeiro", "Minas Gerais", "Bahia", "Paraná"]
+
+## Menu
+
+## Gerar dados aleatórios
+
+## Programa
+
+
+
