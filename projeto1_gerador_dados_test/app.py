e70ec85ba45c98efb8bb3b83e11a056eec9c38a1
saulosgil <saulosgil@hotmail.com>
2024-05-28 20:26:09 -0300

    update: app.py

--- projeto1_gerador_dados_test/app.py
+++ projeto1_gerador_dados_test/app.py
@@ -1,6 +1,5 @@
 ## Bibliotecas
 import random
-import os
 
 ## Listas 
 # Nomes
@@ -43,10 +42,77 @@ states = ["São Paulo",
           ]
 
 ## Menu
+def menu():
+    print('BEM-VINDO AO GERADOR DE DADOS')
+    print('-' * 75)
+    print('Escolha uma ou mais opções separado por vírgula.')
+    print('-' * 75)
+    print('[1] - Nome')
+    print('[2] - E-mail')
+    print('[3] - Telefone')
+    print('[4] - Cidade')
+    print('[5] - Estado')
+    print('[SAIR] - Para Finalizar')
+    
 
 ## Gerar dados aleatórios
+def generate_random_data(choices):
+    """
+    Generates random data based on the specified data types.
+    
+    Args:
+        choices (list): A list of strings representing the desired data types.
+                           Valid options are: "name", "email", "phone", "city", "state".
+    
+    Returns:
+        list: A list of randomly generated data based on the specified data types.
+    """
+    random_data = []
+    
+    for choice in choices:
+        if choice == 1:
+            random_data.append(random.choice(names))
+        elif choice == 2:
+            random_data.append(random.choice(emails))
+        elif choice == 3:
+            random_data.append(random.choice(phone_numbers))
+        elif choice == 4:
+            random_data.append(random.choice(cities))
+        elif choice == 5:
+            random_data.append(random.choice(states))
+        else:
+            random_data.append("Seleção inválida, tente novamente.")
+    
+    return random_data
+
+## Salvar lista em um arquivo.txt
+def save_to_file(data):
+    with open('random_data.txt', 'a') as file:
+        for item in data:
+            file.write(str(item) + '\n')
 
 ## Programa
+if __name__ == "__main__":
+    while True:
+        menu()
+        user_input = input("Digite uma ou mais opções (separado por vígulas): ")
+        print('-' * 75)
 
+        if user_input.upper() == 'SAIR':
+            print("Finalizando o programa.")
+            break
 
+        try:
+            user_choices = [int(choice) for choice in user_input.split(',')]
+            random_data = generate_random_data(user_choices)
+            
 
+            save_option = input("Você deseja salvar esta informação em um arquivo? (s/n)").lower()
+            if save_option == 's':
+                save_to_file(random_data)
+                print("Salvo no arquivo: 'random_data.txt'")
+            
+            print(random_data)
+
+        except ValueError:
+            print("Entrada inválida. Por favor insira um número ou \"Sair\".")
